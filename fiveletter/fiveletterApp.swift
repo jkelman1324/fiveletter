0321a6f485bbdf2999622908e0f93e03f2010720
jkelman1324 <51901448+jkelman1324@users.noreply.github.com>
2022-10-24 23:29:46 -0700

    Initial Commit

---
+++ fiveletter/fiveletterApp.swift
@@ -0,0 +1,17 @@
+//
+//  fiveletterApp.swift
+//  fiveletter
+//
+//  Created by Jake Kelman on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct fiveletterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
